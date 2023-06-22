2543c009b9914cf9c87d95891195f7fca99552f7
Clau4705 <claudiatorres.silvaaa5@gmail.com>
2023-06-22 10:01:50 -0300

    Projeto-calc

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function insertValues(num){
+    const input = document.getElementById('input-value').value
+    document.getElementById('input-value').value = input + num
+}
+
+function calculate() {
+    const input = document.getElementById('input-value').value
+    document.getElementById('input-value').value = eval(input)
+}
+
+function clearInput(){
+    document.getElementById('input-value').value = ""
+}
+
+function invertValue() {
+    calculate()
+    const input = document.getElementById('input-value').value
+    document.getElementById('input-value').value = input * -1
+}
+
+function percentValue() {
+    calculate()
+    const input = document.getElementById('input-value').value
+    document.getElementById('input-value').value = input * 0.1
+    
+}
+
+
+
